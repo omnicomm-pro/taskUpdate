b46790c1c3c2b276d65fec67c3bd8ee8a799122b
stayfuneral <stay.funeral@gmail.com>
2019-09-03 10:56:26 +0700

    модифицировал init.php
    Предыдущий код вызывал зацикливание, в результате чего весь портал начинал дико тормозить.

--- init.php
+++ init.php
@@ -1,37 +1,42 @@
 <?php
-// Обработчик, срабатывает при создании/делегировании задачи, если ответственным назначена Хлопцева О.Е., добавляет в наблюдатели сотрудников её отдела
-AddEventHandler('tasks', 'OnTaskAdd', ['TaskHandler', 'taskUpdate']);
-AddEventHandler('tasks', 'OnTaskUpdate', ['TaskHandler', 'taskUpdate']);
 
-class TaskHandler {
-	function taskUpdate($taskID) {
-		global $USER;
-		$usersID = [];
-		$Filter = [
-			'ACTIVE' => true,
-			'UF_DEPARTMENT' => 83
-		];
-		$Select = [
-			'FIELDS' => ['ID']
-		];
-		$Users = CUser::GetList($by='ID', $order='ASC', $Filter, $Select);
-		while($user = $Users->Fetch()) {
-			$usersID[] = $user['ID'];
-		}
-		if(CModule::IncludeModule('tasks')) {
-			
-			$rsTask = CTasks::GetByID($taskID);
-			$arTask = $rsTask->GetNext();
-			$responsibleID = $arTask['RESPONSIBLE_ID'];
-			if($responsibleID) {
-				$key = array_search($responsibleID, $usersID);
-				unset($usersID[$key]);
-			}
-			$updFields = [
-				'AUDITORS' => $usersID
-			];
-			$TASKS = new CTasks;
-			$taskUpdate = $TASKS->Update($taskID, $updFields);
-		}
+// Получение списка пользователей отдела исполнителя задачи
+global $USER;
+$usersID = [];
+$Filter = [
+	'ACTIVE' => true,
+	'UF_DEPARTMENT' => 83
+];
+$Select = [
+	'FIELDS' => ['ID']
+];
+$Users = CUser::GetList($by='ID', $order='ASC', $Filter, $Select);
+while($user = $Users->Fetch()) {
+	$usersID[] = $user['ID'];
+}
+
+// Событие перед созданием задачи
+AddEventHandler("tasks", "OnBeforeTaskAdd", "OnBeforeTaskAdd"); 
+function OnBeforeTaskAdd(&$arFields) {
+	global $usersID;
+	$responsibleID = $arFields['RESPONSIBLE_ID'];
+	if($responsibleID) {
+		$key = array_search($responsibleID, $usersID);
+		unset($usersID[$key]);
 	}
+	$arFields['AUDITORS'] = $usersID; 
+	return $arFields;
+}
+
+//Событие перед изменением задачи
+AddEventHandler("tasks", "OnBeforeTaskUpdate", "OnBeforeTaskUpdate"); 
+function OnBeforeTaskUpdate($taskID, &$arFields) {
+	global $usersID;
+	$responsibleID = $arFields['RESPONSIBLE_ID'];
+	if($responsibleID) {
+		$key = array_search($responsibleID, $usersID);
+		unset($usersID[$key]);
+	}
+	$arFields['AUDITORS'] = $usersID; 
+	return $arFields;
 }
